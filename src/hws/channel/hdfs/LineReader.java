8e0a5abfd172c4d5d171dd40c684b732d51fa962
Vinícius Vitor dos Santos Dias <viniciusvdias@hotmail.com>
2015-01-15 00:57:14 -0200

    added directory support to HDFS LineReader

--- src/hws/channel/hdfs/LineReader.java
+++ src/hws/channel/hdfs/LineReader.java
@@ -19,6 +19,10 @@ package hws.channel.hdfs;
 
 import java.io.*;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.hadoop.fs.FileSystem;
@@ -33,54 +37,72 @@ import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import hws.core.ChannelDeliver;
 
 public class LineReader extends ChannelDeliver<String>{
-    private PrintWriter out;
-    private FSDataInputStream reader;
-    private long totalBytes;
-    private long beginPos;
-    private long endPos;
-    private long pos;
-    private StringBuffer buffer;
+   private PrintWriter out;
+   private FileSystem fileSystem;
+   private FSDataInputStream reader;
+   private Map<String, FileStatus> files;
+   private SortedSet<String> fileSet;
+   private long totalBytes;
+   private long beginPos;
+   private long endPos;
+   private long pos;
+   private StringBuffer buffer;
 
-    public void start(){
-       
-        super.start();
-        try{
-           out = new PrintWriter(new BufferedWriter(new FileWriter("/home/hadoop/rcor/yarn/channel-deliver-"+channelName()+".out")));
-           out.println("Starting channel deliver: "+channelName()+" instance "+instanceId());
-           out.flush();
-        }catch(IOException e){
-           e.printStackTrace();
-        }
-         buffer = new StringBuffer();
-         try{
+   public void start(){
+      super.start();
+      try{
+         out = new PrintWriter(new BufferedWriter(new FileWriter("/home/hadoop/rcor/yarn/channel-deliver-"+channelName()+".out")));
+         out.println("Starting channel deliver: "+channelName()+" instance "+instanceId());
+         out.flush();
+      }catch(IOException e){
+         e.printStackTrace();
+      }
+
+      files = new HashMap<String, FileStatus>();
+      fileSet = new TreeSet<String>();
+      buffer = new StringBuffer();
+      try{
          Configuration conf = new Configuration();
-//       conf.setBoolean("fs.hdfs.impl.disable.cache", true);
-         FileSystem fileSystem = FileSystem.get(conf);
+         //       conf.setBoolean("fs.hdfs.impl.disable.cache", true);
+         fileSystem = FileSystem.get(conf);
 
-         String path = attribute("path");
+         String pathAttr = attribute("path");
          //check if starts with "hdfs://"
-         if(!path.startsWith("hdfs://")){
-            path = "hdfs://"+path;
+         if(!pathAttr.startsWith("hdfs://")){
+            pathAttr = "hdfs://"+pathAttr;
          }
-         out.println("Opening path: " + path);
+         out.println("Opening path: " + pathAttr);
          out.flush();
+         Path path = new Path(pathAttr);
+         ///reader = fileSystem.open(path);
+
          ///TODO FileSystem:: public boolean isDirectory(Path f)
-         reader = fileSystem.open(new Path(path));
+         if(fileSystem.isDirectory(path)){
+            //totalBytes
+            //TODO a list of files
+            FileStatus[] status = fileSystem.listStatus(path);
+            totalBytes = 0;
+            for(int i=0;i<status.length;i++){
+               totalBytes += status[i].getLen();
+               fileSet.add(status[i].getPath().getName());
+               files.put(status[i].getPath().getName(), status[i]);
+               out.println("fileLen: " + status[i].getLen());
+               out.flush();
+               out.println("file path: "+status[i].getPath().toString());
+               out.flush();
+            }
+         }else{
+            //a file
+            FileStatus status = fileSystem.getFileStatus(path);
+            files.put(path.getName(), status);
+            fileSet.add(path.getName());
+            totalBytes = status.getLen();
+            out.println("fileLen: " + status.getLen());
+            out.flush();
+            out.println("file path: "+status.getPath().toString());
+            out.flush();
+         }
 
-         //totalBytes
-         //a file
-         FileStatus status = fileSystem.getFileStatus(new Path(path));
-         totalBytes = 0;
-         totalBytes += status.getLen();
-         out.println("fileLen: " + status.getLen());
-         out.flush();
-         out.println("file path: "+status.getPath().toString());
-         out.flush();
-
-         //TODO a list of files
-         FileStatus status[] = fileSystem.get
-
-         
          //split
          int split = (int)Math.ceil((double)(totalBytes)/(double)(super.numFilterInstances()));
          beginPos = (super.instanceId())*split;
@@ -97,65 +119,68 @@ public class LineReader extends ChannelDeliver<String>{
          out.println("pos: "+pos);
          out.flush();
 
+         //loading first file
+         nextFile();
+
          //deliverLine
          String line = readLine();
-         
+
          while( line != null ){
-               out.println("processing line: "+line);
-               out.flush();
-               deliver( line );
-               line = readLine();
-         }
-         }catch(IOException e){
-            out.println("EXCEPTION: "+e.getMessage());
+            out.println("processing line: "+line);
             out.flush();
-            e.printStackTrace();
+            deliver( line );
+            line = readLine();
          }
-    }
+      }catch(IOException e){
+         out.println("EXCEPTION: "+e.getMessage());
+         out.flush();
+         e.printStackTrace();
+      }
+   }
 
    public String readLine() throws IOException{
-            if(pos>=endPos) return null;
-            if(reader==null) throw new IOException();
-            else {
-               while(true){
-                  byte ch;
-                  try{
-                     pos++; //position tracker
-                     ch = reader.readByte();
-                  }catch(EOFException e){
-                     String str = buffer.toString();
-                     buffer = new StringBuffer(); //TODO reset the same StringBuffer object
-                     //nextFile(); //TODO nextFile
-                     return str;
-                  }
-
-                  /*out.println("ch: "+((char)ch));
-                  out.flush();
-                  out.println("pos: "+pos);
-                  out.flush();*/
-                  if(ch==((byte)'\n')){
-                     String str = buffer.toString();
-                     buffer = new StringBuffer(); //TODO reset the same StringBuffer object
-                     return str;
-                  }else if(ch==((byte)'\r')){
-                     pos++; //position tracker
-                     ch = reader.readByte();
-                     if(ch==((byte)'\n')){
-                        String str = buffer.toString();
-                        buffer = new StringBuffer(); //TODO reset the same StringBuffer object
-                        return str;
-                     }else {
-                        String str = buffer.toString();
-                        buffer = new StringBuffer(); //TODO reset the same StringBuffer object
-                        buffer.append((char)ch);
-                        return str;
-                     }
-
-                  }else {
-                     buffer.append((char)ch);
-                  }
-               }
+      if(pos>=endPos) return null;
+      if(reader==null) throw new IOException();
+      else {
+         while(true){
+            byte ch;
+            try{
+               pos++; //position tracker
+               ch = reader.readByte();
+            }catch(EOFException e){
+               String str = buffer.toString();
+               buffer = new StringBuffer(); //TODO reset the same StringBuffer object
+               nextFile(); //nextFile
+               return str;
             }
+
+            /*out.println("ch: "+((char)ch));
+              out.flush();
+              out.println("pos: "+pos);
+              out.flush();*/
+            if(ch==((byte)'\n')){
+               String str = buffer.toString();
+               buffer = new StringBuffer(); //TODO reset the same StringBuffer object
+               return str;
+            }else if(ch==((byte)'\r')){
+               pos++; //position tracker
+               ch = reader.readByte();
+               if(ch==((byte)'\n')){
+                  String str = buffer.toString();
+                  buffer = new StringBuffer(); //TODO reset the same StringBuffer object
+                  return str;
+               }else {
+                  String str = buffer.toString();
+                  buffer = new StringBuffer(); //TODO reset the same StringBuffer object
+                  buffer.append((char)ch);
+                  return str;
+               }
+
+            }else {
+               buffer.append((char)ch);
+            }
+         }
+      }
    }
 
    private boolean nextFile() throws IOException{
@@ -178,8 +203,8 @@ public class LineReader extends ChannelDeliver<String>{
          reader = fileSystem.open(files.get(nextFileName).getPath());
          if(filePos>0){ //if the position to read is in the middle of the file, skip the previous line.
             reader.seek(filePos-1);
-            
-            read();//go to the next line of the file
+
+            readLine();//go to the next line of the file
          }
          return true;
       }else {
@@ -188,31 +213,31 @@ public class LineReader extends ChannelDeliver<String>{
       }
    }
 
-	public void finish(){
-        super.finish();
-        out.println("attribute: 'wait' = "+attribute("wait"));
-        if("true".equals(attribute("wait"))){
-           out.println("Waiting for producers to end");
-           out.flush();
-           /*try {
-              //latch.await(); //await the input threads to finish
-           }catch(InterruptedException e){
-              // handle
-              out.println("Waiting ERROR: "+e.getMessage());
-              out.flush();
-           }*/
-        }
-        //try{
-           out.println("Finishing channel deliver: "+channelName()+" instance "+instanceId());
-           out.flush();
-           out.close();
-        /*}catch(IOException e){
-           e.printStackTrace();
+   public void finish(){
+      super.finish();
+      out.println("attribute: 'wait' = "+attribute("wait"));
+      if("true".equals(attribute("wait"))){
+         out.println("Waiting for producers to end");
+         out.flush();
+         /*try {
+         //latch.await(); //await the input threads to finish
+         }catch(InterruptedException e){
+         // handle
+         out.println("Waiting ERROR: "+e.getMessage());
+         out.flush();
+         }*/
+      }
+      //try{
+      out.println("Finishing channel deliver: "+channelName()+" instance "+instanceId());
+      out.flush();
+      out.close();
+      /*}catch(IOException e){
+        e.printStackTrace();
         }*/
-	}
+   }
 
-    public void onProducersHalted(){
-       out.println("PRODUCERS HALTED!!");
-       out.flush();
-    }
+   public void onProducersHalted(){
+      out.println("PRODUCERS HALTED!!");
+      out.flush();
+   }
 }
